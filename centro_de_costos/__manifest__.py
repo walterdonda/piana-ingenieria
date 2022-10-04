b2ac18f7ae10ad9afffb1244e014c5be27c92278
Walter Donda <walteroscardonda@gmail.com>
2022-10-04 17:30:03 -0300

    Se crea el manifest para producción

--- centro_de_costos/__manifest__.py
+++ centro_de_costos/__manifest__.py
@@ -1,30 +1,34 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Centro de costos para hacer seguimiento de proyectos de ingeniería",
+    "name": "Centro de costos y proyectos ingeniería",
     "summary": """
         Módulo que permite agregar el concepto de centro de costos a odoo 
         """,
     "description": """
-        Este módulo permite hacer el seguimiento de la facturación de c/u de los proyectos/departamentos de la empresa.
+        Este módulo permite hacer el seguimiento de la facturación de c/u de la empresa.
+        
         A cada proyecto se le asigna un centro de costo o una jerarquía del mismo. 
         Para esto se modifica el modelo de odoo cuenta analítica y se le agregan los siguientes campos:
+        
         budget_project --> Presupuesto del proyecto
+        
         margin_project --> Margen económico del proyecto
+        
         outstanding_invoice_amount --> Monto pendiente a facturar
-        Al final del proyecto el monto pendiente de facturar debe ser 0, el monto pendiente de facturar es la resta entre
-        el presupuesto (variable) y la diferencia resultante entre las facturas a cliente menos las facturas de proveedores (balance)
+        
+        Al final del proyecto el monto pendiente de facturar en el centro de costo debe ser 0.
     """,
     "author": "Walter Donda",
     "category": "Services/Ingenieria",
     "version": "1.0",
-    # any module necessary for this one to work correctly
-    "depends": ["analytic"],
-    # always loaded
+    # Este módulo depende de los siguientes módulos de odoo 15
+    "depends": ["base", "account", "analytic"],
+    # Se cargan los datos de acceso
     "data": [
         "security/ir.model.access.csv",
         "views/views.xml",
     ],
-    # only loaded in demonstration mode
+    # Datos demos
     "demo": [
         "demo/demo.xml",
     ],
