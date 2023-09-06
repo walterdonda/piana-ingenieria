ba212d62b1074ed1c1060250b311782c901def05
Walter Donda <walteroscardonda@gmail.com>
2023-09-06 20:37:38 -0300

    Agrego fecha de cancelación a las facturas para aprovechar el campo en centro de costos

--- centro_de_costos/models/account_analytic.py
+++ centro_de_costos/models/account_analytic.py
@@ -166,3 +166,21 @@ class CentroDeCostos(models.Model):
                 vna = record.margin_project
             record.tir_no_per = tir 
             record.vna = vna
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    payment_date = fields.Date('Payment Date')
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    def reconcile(self):
+        res = super(AccountMoveLine, self).reconcile()
+        payment_date = None
+        for rec in self:
+            if rec.move_id.payment_id:
+                payment_date = rec.move_id.payment_id.date
+        for rec in self:
+            if rec.move_id.move_type == 'out_invoice':
+                rec.move_id.payment_date = payment_date
+        return res
